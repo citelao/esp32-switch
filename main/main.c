0ca80dd32041def930642feb634c4330bf9ded9d
Ben Stolovitz <ben@stolovitz.com>
2025-01-06 21:54:06 -0500

    gpio, but panics...

--- main/main.c
+++ main/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <esp_log.h>
 #include <driver/ledc.h>
+#include <driver/gpio.h>
 #include <esp_timer.h>
 #include <led_strip.h>
 
@@ -9,7 +10,11 @@ static const char *TAG = "CITELAO_ESP32_SWITCH";
 // It's written on the ESP32 board :)
 static const uint8_t LED_GPIO_PIN = 8;
 
+// BOOT button, via switch_driver.h
+static const uint8_t SWITCH_GPIO_PIN = GPIO_NUM_9;
+
 static led_strip_handle_t led_strip = NULL;
+static bool isPressed = false;
 
 void blink(void *arg)
 {
@@ -17,13 +22,40 @@ void blink(void *arg)
     ESP_LOGI(TAG, "Blinking %s", led_state ? "on" : "off");
 
     led_state = !led_state;
-    ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, 0, led_state ? 255 : 0, 0, 0));
+    ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, 0, led_state ? 255 : 0, isPressed ? 255 : 0, 0));
     ESP_ERROR_CHECK(led_strip_refresh(led_strip));
+}
+
+static void IRAM_ATTR switch_pressed(void *arg)
+{
+    isPressed = !isPressed;
+    // ESP_LOGI(TAG, "Switch %s", isPressed ? "pressed" : "released");
 }
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "Hello world!");
+
+    // Configure the BOOT button as input
+    // https://github.com/espressif/esp-zigbee-sdk/blob/main/examples/common/switch_driver/src/switch_driver.c
+    uint64_t mask = 0;
+    mask |= 1ULL << SWITCH_GPIO_PIN;
+    gpio_config_t io_conf = {
+        .intr_type = GPIO_INTR_LOW_LEVEL,
+        .mode = GPIO_MODE_INPUT,
+        .pin_bit_mask = 1ULL << LED_GPIO_PIN,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+    };
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+
+    ESP_LOGI(TAG, "Configuring switch on GPIO %d", SWITCH_GPIO_PIN);
+
+    const int intr_alloc_flags = 0; // default interrupt flag?
+    ESP_ERROR_CHECK(gpio_install_isr_service(intr_alloc_flags));
+    ESP_ERROR_CHECK(gpio_isr_handler_add(SWITCH_GPIO_PIN, &switch_pressed, NULL));
+
+    ESP_LOGI(TAG, "Configuring LED on GPIO %d", LED_GPIO_PIN);
 
     // https://components.espressif.com/components/espressif/led_strip
     led_strip_config_t strip_config = {
