747ddb62564564a114824ce867650a00c27babeb
Ben Stolovitz <ben@stolovitz.com>
2025-01-06 22:31:24 -0500

    button works lol

--- main/main.c
+++ main/main.c
@@ -41,21 +41,18 @@ void app_main(void)
     uint64_t mask = 0;
     mask |= 1ULL << SWITCH_GPIO_PIN;
     gpio_config_t io_conf = {
-        .intr_type = GPIO_INTR_LOW_LEVEL,
+        .intr_type = GPIO_INTR_ANYEDGE,
         .mode = GPIO_MODE_INPUT,
-        .pin_bit_mask = 1ULL << LED_GPIO_PIN,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pin_bit_mask = mask,
+        .pull_down_en = GPIO_PULLDOWN_ENABLE,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
     };
     ESP_ERROR_CHECK(gpio_config(&io_conf));
 
     ESP_LOGI(TAG, "Configuring switch on GPIO %d", SWITCH_GPIO_PIN);
 
-    const int intr_alloc_flags = 0; // default interrupt flag?
-    ESP_ERROR_CHECK(gpio_install_isr_service(intr_alloc_flags));
+    ESP_ERROR_CHECK(gpio_install_isr_service(ESP_INTR_FLAG_LEVEL1));
     ESP_ERROR_CHECK(gpio_isr_handler_add(SWITCH_GPIO_PIN, &switch_pressed, NULL));
-
-    ESP_LOGI(TAG, "Configuring LED on GPIO %d", LED_GPIO_PIN);
 
     // https://components.espressif.com/components/espressif/led_strip
     led_strip_config_t strip_config = {
@@ -79,13 +76,11 @@ void app_main(void)
 
     ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
 
-    esp_timer_create_args_t periodic_timer_args = {
-        .callback = &blink,
-        .arg = NULL,
-        .dispatch_method = ESP_TIMER_TASK,
-        .name = "periodic_timer"
-    };
-    esp_timer_handle_t periodic_timer;
-    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
-    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 1000000)); // 1 second
+    ESP_LOGI(TAG, "Configuring LED on GPIO %d", LED_GPIO_PIN);
+
+    while (1)
+    {
+        blink(NULL);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
 }
